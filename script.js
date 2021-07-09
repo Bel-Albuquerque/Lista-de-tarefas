4d96e37de7be3fbf8f5130795faedbb5cea34832
Bel Albuquerque <mariaisabelalbuquerque@gmail.com>
2021-07-09 14:43:17 -0300

    requisito 5

---
+++ script.js
@@ -0,0 +1,30 @@
+function createButton(textInButton) {
+  let paiButton = document.querySelector('#buttonInput')
+  let button = document.createElement('button')
+  let idButton = 'criar-tarefa'
+  button.innerHTML = textInButton
+  button.id = idButton
+  paiButton.appendChild(button)
+}
+createButton('Tarefa')
+
+let valorInput
+let input = document.querySelector("#texto-tarefa")
+let buttonInput = document.querySelector('#criar-tarefa')
+
+function inputText(event) {
+valorInput = event.target.value
+}
+
+input.addEventListener('input', inputText)
+
+function createLiTarefa(event) {
+  let paiLi = document.querySelector("#lista-tarefas")
+  let li = document.createElement('li')
+  li.classList.add('lista')
+  li.innerHTML = valorInput
+  paiLi.appendChild(li)
+  input.value =''
+}
+
+buttonInput.addEventListener('click',createLiTarefa)
